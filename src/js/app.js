15412d9faa5f3d78357d8e87f8b403091a4f7792
shahrizoda <shahrizodadeveloping@gmail.com>
2022-08-07 21:26:36 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,19 @@ import * as functions from "./modules/functions.js";
 
 functions.isWebp();
 
-// import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, {Pagination } from 'swiper';
 
-// const swiper = new Swiper();
+const swiper = new Swiper();
+
+new Swiper(".intro__swiper", {
+    modules: [Pagination],
+    spaceBetween: 20,
+    slidesPerView: 1,
+    pagination: {
+      el: ".intro__pagination",
+      clickable: true,
+    },
+});
 
 const menu = document.querySelector('.menu');
 const headerContent = document.querySelector('.header__content');
