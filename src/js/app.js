f322477f8452710736b1d66c8f5e139af135c8bf
shahrizoda <shahrizodadeveloping@gmail.com>
2022-08-09 23:15:02 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import * as functions from "./modules/functions.js";
 
 functions.isWebp();
 
-import Swiper, {Pagination } from 'swiper';
+import Swiper, {Pagination, FreeMode } from 'swiper';
 
 const swiper = new Swiper();
 
@@ -16,6 +16,22 @@ new Swiper(".intro__swiper", {
     },
 });
 
+new Swiper(".choice__swiper", {
+  spaceBetween: 20,
+  slidesPerView: 1,
+  loop: true,
+  breakpoints:{
+    992:{
+      modules: [FreeMode],
+      slidesPerView: 'auto',
+      freeMode: true,
+    },
+    576:{
+      slidesPerView: 2,
+    }
+  }
+});
+
 const menu = document.querySelector('.menu');
 const headerContent = document.querySelector('.header__content');
 const body = document.querySelector('body');
@@ -26,3 +42,22 @@ menu.addEventListener('click', (e)=>{
     menu.classList.toggle('active')
 })
 
+// const cursor = document.querySelectorAll('.choice__custom');
+// const choiseItem = document.querySelectorAll('.choice__swiper-slide');
+// choiseItem.forEach(item=>{
+//   item.addEventListener('mousemove', e => {
+//     cursor.forEach(cursor => {
+//       cursor.setAttribute("style", "top: "+(e.clientY)+"px; left: "+(e.clientX)+"px;")
+//     });
+//   })
+// })
+
+const choiseItem = document.querySelectorAll('.choice__swiper-slide');
+choiseItem.forEach(item=>{
+  const cursor = item.querySelector('.choice__custom');
+  item.addEventListener('mousemove', e => {
+    const y = e.offsetY;
+    const x = e.offsetX - cursor.clientWidth; 
+    cursor.setAttribute("style", "top: "+(y)+"px; left: "+ (x)+"px;")
+  })
+})
